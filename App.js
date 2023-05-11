9bce571a4469190731a012b8da5f12ccf8f5896f
Sang Uk Jung <83222288+JSU0318@users.noreply.github.com>
2023-05-11 15:34:20 +0900

    RNTest

--- App.js
+++ App.js
@@ -1,20 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { TextInput } from "react-native-gesture-handler";
+import { Button } from "react-native-web";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ padding: 50 }}>
+      <View style={{ backgroundColor: "red", width: 100, height: 100, justifyContent: "center", alignItems: "center" }}>
+        {/* <Text>List of goals!!</Text> */}
+        <Text>1</Text>
+      </View>
+      <View
+        style={{ backgroundColor: "blue", width: 100, height: 100, justifyContent: "center", alignItems: "center" }}
+      >
+        {/* <Text>List of goals!!</Text> */}
+        <Text>2</Text>
+      </View>
+      <View
+        style={{ backgroundColor: "green", width: 100, height: 100, justifyContent: "center", alignItems: "center" }}
+      >
+        {/* <Text>List of goals!!</Text> */}
+        <Text>3</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  appContainer: { padding: 20 },
+  inputContainer: { flexDirection: "column", justifyContent: "space-between" },
 });
+{
+  /* <View style={styles.inputContainer}>
+        <TextInput style={styles.textInput} placeholder="Your Course Goal!" />
+        <Button title="ADD Goal!!" /> */
+}
